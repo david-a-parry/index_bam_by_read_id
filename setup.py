77e83acac92d95bb3274df12a4459c849ad8aa1a
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-08-08 12:44:24 +0100

    update setup tags

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name = "index_bam_by_read_id",
     packages = [""],
-    version = "0.0.2",
+    version = "0.0.3",
     description = "Sort, index and search BAM files by read ID",
     author = "David A. Parry",
     author_email = "david.parry@ed.ac.uk",
     url = "https://github.com/gantzgraf/index_bam_by_read_id",
-    download_url = 'https://github.com/gantzgraf/index_bam_by_read_id/archive/0.0.2.tar.gz',
+    download_url = 'https://github.com/gantzgraf/index_bam_by_read_id/archive/0.0.3.tar.gz',
     license='MIT',
     test_suite='nose.collector',
     tests_require=['nose'],
@@ -16,7 +19,8 @@ setup(
           'pysam',
       ],
     classifiers = [
-        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
